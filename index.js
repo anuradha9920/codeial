b628395378d83ec1923c3926dae298b2a4a9ae78
anuradha9920 <anuradha9920@gmail.com>
2020-08-05 18:10:45 +0530

    crested controller

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 8000;
 
 
+// use express router
+
+app.use('/',require('./routes'));
+
+
 app.listen(port,(err)=>{
     if(err){
         console.log(`ERROR in running the server: ${err}`);
